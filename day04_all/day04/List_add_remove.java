3c8561bc5f63eb6c9ef349a2f56a83d5e17e9972
wanlong1234567 <2531165965@qq.com>
2018-03-09 15:54:32 +0800

    list的add的方法和remove的方法

---
+++ day04_all/day04/List_add_remove.java
@@ -0,0 +1,41 @@
+package day04;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * List提供了一对重载的add,remove方法
+ * 也可以通过下标操作元素
+ * @author adminitartor
+ *
+ */
+public class List_add_remove {
+	public static void main(String[] args) {
+		List<String> list 
+			= new ArrayList<String>();		
+		list.add("one");
+		list.add("two");
+		list.add("three");
+		list.add("four");
+		System.out.println(list);
+		/*
+		 * [one,2,two,three,four]
+		 */
+		list.add(1, "2");
+		System.out.println(list);
+		
+		/*
+		 * [one,2,three,four]
+		 */
+		String old = list.remove(2);
+		System.out.println(list);
+		System.out.println("被删除元素:"+old);
+		
+	}
+}
+
+
+
+
+
+
